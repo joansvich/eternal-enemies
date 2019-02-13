dea2a1b213051982b48b78ea606a3e9d42f1a95a
Joan Sánchez <joansvich@gmail.com>
2019-02-13 14:43:45 +0100

    game logic done

--- main.js
+++ main.js
@@ -14,7 +14,44 @@ const main = () => {
       <h1>Hello</h1>
       <button>Start</button>
     `);
+    const startButton = document.querySelector('button');
+    startButton.addEventListener('click',buildGameScreen);
+  };
+
+  const buildGameScreen = () => {
+    const gameScreen = buildDom(`
+      <section class="game-screen">
+        <canvas></canvas>
+      </section>
+    `);
+    
+    const width = document.querySelector('.game-screen').offsetWidth;
+    const height = document.querySelector('.game-screen').offsetHeight;
+
+    const canvasElement = document.querySelector('canvas');
+
+    canvasElement.setAttribute('width',width);
+    canvasElement.setAttribute('height',height);
+
+    setTimeout(buildGameOver,3000);
+
+    const game = new Game(canvasElement);
+    game.startLoop();
+
+  };
+
+  const buildGameOver = () => {
+    const gameOverScreen = buildDom(`
+      <section class="game-over">
+        <h1>Game Over Screen</h1>
+        <button>Restart</button>
+      </section>
+    `) 
+    const restartButton = document.querySelector('button');
+  
+    restartButton.addEventListener('click',buildGameScreen);
   }
+
 
   buildSplashScreen();
 }
