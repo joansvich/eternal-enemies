20ad32c5bc403b5e052b7b46e60447eb72034fcb
Joan Sánchez <joansvich@gmail.com>
2019-02-13 14:05:58 +0100

    Build splash screen

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const main = () => {
+  
+
+  const buildDom = (html) => {
+    const main = document.querySelector('main');
+    main.innerHTML = html;
+    return main;
+  }
+
+  const buildSplashScreen = () => {
+    const splashScreen = buildDom(`
+      <h1>Hello</h1>
+      <button>Start</button>
+    `);
+  }
+
+  buildSplashScreen();
+}
+
+
+window.addEventListener('load',main);
